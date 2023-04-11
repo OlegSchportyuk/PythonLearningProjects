8bc736f69e16b670fdd4006b83ff7009156bc1da
OlegSchportyuk <oleg.schportyuk@yandex.ru>
2023-04-11 20:51:06 +0300

    1

---
+++ Erik_Metiz/alien_invasion/ship.py
@@ -0,0 +1,37 @@
+import pygame
+
+class Ship():
+    """Класс для управления кораблем."""
+
+    def __init__(self, screen, settings):
+        """Инициализирует корабль и задает его начальную позицию."""
+        self.screen = screen
+        self.settings = settings
+        self.screen_rect = screen.get_rect()
+
+        # Загружает изображение корабля и получает прямоугольник.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        # Каждый новый корабль появляется у нижнего края экрана.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Сохранение вещественной координаты центра корабля.
+        self.x = float(self.rect.x)
+
+        # Флаги перемещения
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Обновляет позицию корабля с учетом флагов."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Обновление атрибута rect на основании self.x.
+        self.rect.x = self.x
+
+    def blitme(self):
+        """Рисует корабль в текущей позиции."""
+        self.screen.blit(self.image, self.rect)
